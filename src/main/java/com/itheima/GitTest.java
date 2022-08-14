b6e02b68f07e5e18c3d10e0d1fac08c00f748f4e
Jade <Jade@itheima.com>
2022-08-14 14:59:38 +0800

    Idea hot-fig1 commit

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("美好的生活");
         System.out.println("美好的生活313");
+        System.out.println("我是hot-fig");
+        System.out.println("我是hot-fig，存入本地库");
+
     }
 }
